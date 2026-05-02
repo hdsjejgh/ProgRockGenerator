e2e4c03bdd5a598a8ea2ad10cb7a4676fd86f6c4
hdsjejgh <140638415+hdsjejgh@users.noreply.github.com>
2026-05-02 18:15:20 -0400

    added basic event system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import music21
 import os
+
 from music21 import midi
 #music21.configure.run()
 
@@ -20,4 +21,51 @@ def list_instruments(midi_obj):
         print(p.partName)
 
 test_midi = open_midi(files[3])
-list_instruments(test_midi)
+
+def make_note(element, pitch=None):
+    if pitch is None:
+        pitch = element.pitch
+
+    start = element.offset
+    duration = element.duration.quarterLength
+    return {
+        "pitch": pitch.midi,
+        "start": start,
+        "end": start + duration,
+        "velocity": getattr(element, "volume", None),
+        "instrument": getattr(element, "instrument", 0)
+    }
+
+
+def get_notes(stream):
+    notes = []
+    for i in stream.flatten():
+        if isinstance(i,music21.note.Note):
+            notes.append(make_note(i))
+
+        elif isinstance(i, music21.chord.Chord):
+            for pitch in i.pitches:
+                notes.append(make_note(i,pitch=pitch))
+
+    return notes
+
+def get_events(notes):
+    events = []
+    for note in notes:
+        events.append({
+            "type":"ON",
+            "pitch":note["pitch"],
+            "time": note["start"]
+        })
+
+        events.append({
+            "type": "OFF",
+            "pitch": note["pitch"],
+            "time": note["end"]
+        })
+    return events
+
+
+notes = get_notes(test_midi)
+events = get_events(notes)
+print(events)
